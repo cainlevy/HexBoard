daac2138644e1def6c7b3c814ca4f09675182670
Lance Ivy <lance@cainlevy.net>
2014-07-07 21:38:37 -0700

    learning about folders and groups in xcode

---
+++ lib/RegularShape.swift
@@ -0,0 +1,28 @@
+//
+//  RegularShape.swift
+//  HexBoard
+//
+//  Created by Lance Ivy on 7/6/14.
+//  Copyright (c) 2014 Lance Ivy. All rights reserved.
+//
+
+import Foundation // for cos() and sin()
+import UIKit
+
+struct RegularShape {
+    var sides: Int
+    
+    var angle: Double {
+        return (2 * pi) / Double(sides)
+    }
+    
+    var points: CGPoint[] {
+        return sides.times().map{ (i) in
+            var degrees = Double(i) * self.angle
+            return CGPoint(
+                x: CGFloat(cos(degrees)),
+                y: CGFloat(sin(degrees))
+            )
+        }
+    }
+}
--- lib/extensions.swift
+++ lib/extensions.swift
@@ -1,31 +1,12 @@
 //
-//  RegularShape.swift
+//  extensions.swift
 //  HexBoard
 //
-//  Created by Lance Ivy on 7/6/14.
+//  Created by Lance Ivy on 7/7/14.
 //  Copyright (c) 2014 Lance Ivy. All rights reserved.
 //
 
-import Foundation // for cos() and sin()
-import UIKit
-
-struct RegularShape {
-    var sides: Int
-    
-    var angle: Double {
-        return (2 * pi) / Double(sides)
-    }
-    
-    var points: CGPoint[] {
-        return sides.times().map{ (i) in
-            var degrees = Double(i) * self.angle
-            return CGPoint(
-                x: cos(degrees),
-                y: sin(degrees)
-            )
-        }
-    }
-}
+import Foundation
 
 let pi = 3.14159 // TODO: where is this supposed to come from?
 
